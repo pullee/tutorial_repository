9c10b24331f9cb7d25f0c9e2d7ad71edb6267a58
Lee Ho Jin <pullee@naver.com>
2016-01-13 17:09:55 +0900

    tutorial version v1.3a

--- tutorial.cpp
+++ tutorial.cpp
@@ -6,6 +6,6 @@ int main()
 	cout << "Hi, This is Tutorial!" << endl << endl;
 	cout << "Wow! I like it!!" << endl;
 	cout << "Fantastic!" << endl;
-
+	cout << "Master branch" << endl;
 	return 0;
 }
